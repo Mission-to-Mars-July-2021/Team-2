7596e4e9f32bd7e004acc9795c1d58e5359814a5
abkuut <abkuut@gmail.com>
2021-07-14 15:03:25 +0100

    Story 6

--- Story_4/Story_4.ino
+++ Story_4/Story_4.ino
@@ -6,10 +6,8 @@ int const FORWARD_LEFT = 11;
 int const REVERSE_LEFT = 12;
 const int LEFT_FEEDBACK = 2; 
 const int RIGHT_FEEDBACK = 3;
-
 volatile int leftcounter = 0;
 volatile int rightcounter = 0;
-  
 
 void setup()
 {
@@ -22,58 +20,75 @@ void setup()
   pinMode(FORWARD_RIGHT, OUTPUT);
   pinMode(ENABLE_RIGHT, OUTPUT);
   pinMode(ENABLE_LEFT, OUTPUT);
-    
 }
-
-void loop()   // this is a loop!!!!
+void loop()
     {    
-  forwards();
   
-  Serial.print("LEFT COUNTER");
-    Serial.println(leftcounter);
-    Serial.print("RIGHT COUNTER");
-  Serial.println(rightcounter);
-
-
-if(leftcounter >= 870) {
-  Serial.println("Counter has reached 870");
+  if(millis() > 10000) {
   stop();
-  delay(100000);
+  delay(1500)
+  exit();
+  }
+  
+    forwards(155,155);
+    
+  if(leftcounter > rightcounter) 
+  {
+    forwards(155,160);
+    
+  }
+  
+  if (rightcounter > leftcounter) 
+  {
+     forwards(165,155);
+  }
+  
+  Serial.print("Left Counter");
+  Serial.println(leftcounter);
+  
+  Serial.print("Right Counter");
+  Serial.print(rightcounter);  
+    
+    if(rightcounter >= 10) {
+     Serial.println("Counter has reached 10");
+  }
 }
-}
+    
+    void LeftMotorISR() 
+    {
+    leftcounter++;
+     }
 
-
-void LeftMotorISR() {
-  leftcounter++;
-}
-
-void RightMotorISR() {
-  rightcounter++;
-}
-
-  void forwards(){
-  digitalWrite(ENABLE_RIGHT, HIGH);
-  digitalWrite(ENABLE_LEFT, HIGH);
+    void RightMotorISR() 
+    {
+    rightcounter++;
+           }             
+                 
+  void forwards(int leftspeed, int rightspeed)
+  {
+  analogWrite(ENABLE_RIGHT, rightspeed);
+  analogWrite(ENABLE_LEFT, leftspeed);
   digitalWrite(FORWARD_RIGHT, HIGH);
   digitalWrite(FORWARD_LEFT, HIGH);
   digitalWrite(REVERSE_LEFT, LOW);
   digitalWrite(REVERSE_RIGHT, LOW);
-}
-  void backwards() {
+           }
+  void backwards() 
+  {
   digitalWrite(ENABLE_RIGHT, HIGH);
   digitalWrite(ENABLE_LEFT, HIGH);
   digitalWrite(FORWARD_LEFT, LOW);
   digitalWrite(FORWARD_RIGHT, LOW);
   digitalWrite(REVERSE_LEFT, HIGH);
   digitalWrite(REVERSE_RIGHT, HIGH);
-}
-void stop() {
+           }
+  void stop() 
+  {
   digitalWrite(ENABLE_RIGHT, LOW);
   digitalWrite(ENABLE_LEFT, LOW);
   digitalWrite(FORWARD_LEFT, LOW);
   digitalWrite(FORWARD_RIGHT, LOW);
-  digitalWrite(REVERSE_LEFT, HIGH);
-  digitalWrite(REVERSE_RIGHT, HIGH);
-  
-}
+  digitalWrite(REVERSE_LEFT, LOW);
+  digitalWrite(REVERSE_RIGHT, LOW);  
+}              
                   
